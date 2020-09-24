90264f9736ad49f2df89946751c9be9ef04c949a
Chris <me@chrisconnelly.dev>
2020-09-24 03:39:12 -0600

    Dynamic Realations

--- app.js
+++ app.js
@@ -6,12 +6,36 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const Event = require('./models/event');
-
+const User = require('./models/user');
 const app = express();
 
-const events = [];
-
 app.use(bodyParser.json());
+
+const events = eventIds => {
+    return Event.find({_id: {$in: eventIds}})
+    .then(events => {
+        return events.map(event => {
+            return { 
+                ...event._doc, 
+                _id: event.id, 
+                creator: user.bind(this, event.creator)  
+            };
+        });
+    })
+    .catch(err => {
+        throw err;
+    });
+}
+
+const user = userId => {
+    return User.findById(userId)
+    .then(user => {
+        return { ...user._doc, _id: user.id };
+    })
+    .catch(err => {
+        throw err;
+    });
+}
 
 app.use(
     "/graphql",
@@ -23,12 +47,14 @@ app.use(
                 description: String!
                 price: Float!
                 date: String!
+                creator: User!
             }
 
             type User {
                 _id: ID!
                 email: String!
                 passwod: String
+                createdEvents: [Event!]
             }
 
             input EventInput {
@@ -62,8 +88,12 @@ app.use(
                 return Event.find()
                 .then(events => {
                     return events.map(event => {
-                        return { ...event._doc };
-                    })
+                        return {
+                            ...event._doc,
+                            _id: event.id,
+                            creator: user.bind(this, event._doc.creator)
+                        };
+                    });
                 })
                 .catch(err => {
                     throw err;
@@ -74,13 +104,25 @@ app.use(
                     title: args.eventInput.title,
                     description: args.eventInput.description,
                     price: +args.eventInput.price,
-                    date: new Date(args.eventInput.date)
+                    date: new Date(args.eventInput.date),
+                    creator: '5f6c5b6d20dd617d1aea8db1'
                 });
+                let createdEvent;
                 return event
                 .save()
                 .then(result => {
-                    console.log(result);
-                    return {...result._doc};
+                    createdEvent = { ...result._doc, _id: result._doc._id.toString() };
+                    return User.findById('5f6c5b6d20dd617d1aea8db1')
+                })
+                .then(user => {
+                    if (!user) {
+                        throw new Error('User Not Found');
+                    }
+                    user.createdEvents.push(event);
+                    return user.save();
+                })
+                .then(result => {
+                    return createdEvent
                 })
                 .catch(err => {
                     console.log(err);
@@ -89,8 +131,13 @@ app.use(
                 return event;
             },
             createUser: args => {
-                return bcrypt
-                .hash(args.userInput.password, 12)
+                return User.findOne({email: args.userInput.email})
+                .then(user => {
+                    if (user) {
+                        throw new Error ('User already exists')
+                    }
+                    return bcrypt.hash(args.userInput.password, 12)
+                })
                 .then(hashedPassword => {
                     const user = new User({
                         email: args.userInput.email,
@@ -99,7 +146,7 @@ app.use(
                     return user.save();
                 })
                 .then(result => {
-                    return{ ...result._doc, _id: result.id };
+                    return{ ...result._doc, password: null,  _id: result.id };
                 })
                 .catch(err => {
                     throw err;
@@ -110,7 +157,7 @@ app.use(
     }),
 );
 
-mongoose.connect('mongodb+srv://<username>:<password>@cluster0.oxtkc.mongodb.net/<dbname>?retryWrites=true&w=majority')
+mongoose.connect('mongodb+srv://web-site:2nq0k7fYb9vVxL7v@cluster0.oxtkc.mongodb.net/events?retryWrites=true&w=majority')
 .then(() => {
 
 })
--- models/user.js
+++ models/user.js
@@ -10,7 +10,7 @@ const userSchema = new Schema({
     password: {
         type: String,
         required: true
-    }
+    },
     createdEvents: [
         {
             type: Schema.Types.ObjectId,
